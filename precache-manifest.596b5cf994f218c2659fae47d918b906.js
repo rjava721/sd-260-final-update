2711fcb307294dfd56e7dba5e4563db22c06074b
Jawad <rjava721@gmail.com>
2020-01-31 10:57:30 -0600

    Updates

---
+++ precache-manifest.596b5cf994f218c2659fae47d918b906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59f5e8a484f6ce6c88a31d2b73f492c",
+    "url": "/sapolor/sd-260-final-update/index.html"
+  },
+  {
+    "revision": "1969c720afe7c2326ebd",
+    "url": "/sapolor/sd-260-final-update/static/css/main.d30a4ac9.chunk.css"
+  },
+  {
+    "revision": "0874e985f5bb039d86f6",
+    "url": "/sapolor/sd-260-final-update/static/js/2.2dc0b4ea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sapolor/sd-260-final-update/static/js/2.2dc0b4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1969c720afe7c2326ebd",
+    "url": "/sapolor/sd-260-final-update/static/js/main.7e244aa5.chunk.js"
+  },
+  {
+    "revision": "3d4bcd49d8019712789d",
+    "url": "/sapolor/sd-260-final-update/static/js/runtime-main.2545e000.js"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
